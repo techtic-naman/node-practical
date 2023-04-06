06119a71f1ce303d35613b937eafacd2bd4dee9b
techtic-naman <naman@techtic.agency>
2023-04-06 15:00:29 +0530

    date object added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const port = process.env.port || 3000
+const birthdate = new Date('1999-11-10').toLocaleDateString();
 http.createServer(function (request, response) {   
-    response.end('birthdate:10-11-1999');  
+    response.end(birthdate);  
 }).listen(port); 
  
