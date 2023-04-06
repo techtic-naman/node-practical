78e724f0f5e7c0b70965501c6e81455ba4f70325
techtic-naman <naman@techtic.agency>
2023-04-06 14:39:47 +0530

    node practical updated

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require('http')
-const hostname = process.env.hostname || '127.0.0.1'
 const port = process.env.port || 3000
 http.createServer(function (request, response) {   
     response.end('birthdate:10-11-1999');  
-}).listen(port,hostname); 
+}).listen(port); 
  
